6134e36b88852b343647cb86afa101a7d31bb819
Glen-Clarence <glenclarence007@gmail.com>
2021-04-29 20:25:14 +0530

    completed two assignments

---
+++ Assign-0101/index.js
@@ -0,0 +1,12 @@
+  //mainString = Sample string
+
+  function containsString (mainString,seachString) {
+    // Check if the substring exists inside the string
+    if(mainString.indexOf(seachString) !== -1){
+        return true;
+    } else{
+        return false;
+    }
+    }
+    
+  
---
+++ Assign-0102/index.js
@@ -0,0 +1,9 @@
+var month = [ "Janauary", "Febrauary","March","April","May","June","July","August","September","October","November","December"];
+function getMonthName(a){
+     getMonth = month[a - 1];
+     console.log('Hello Glen its the month of '+ getMonth);
+}
+
+// Call function in the console!
+    
+
